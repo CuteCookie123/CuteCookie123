74709915080d865855d664057f433ea5bc1e8e88
yingrou <yingrou_wang@163.com>
2018-08-21 17:59:34 +0800

    update login.html

--- pc/js/login.js
+++ pc/js/login.js
@@ -23,4 +23,25 @@ $(document).ready(function(){
         $("#emailLogin-box").hide();
         $("#telLogin-box").show();
     });
+
+    var winHeight = $(document).scrollTop();
+
+    $(window).scroll(function() {
+        var scrollY = $(document).scrollTop();// 获取垂直滚动的距离，即滚动了多少
+
+        if (scrollY > 800 && scrollY < 4500){ //如果滚动距离大于550px则隐藏，否则删除隐藏类
+            $('.bottom-nav').fadeIn();
+        }
+        else {
+            $('.bottom-nav').fadeOut();
+        }
+
+        // if (scrollY > winHeight){ //如果没滚动到顶部，删除显示类，否则添加显示类
+        //     $('.bottom-nav').hide();
+        // }
+        // else {
+        //     $('.bottom-nav').show();
+        // }
+
+    });
 });
