eae6a52cef5da81afbba6d1ca2901632078bb691
yingrou <yingrou_wang@163.com>
2018-08-27 10:23:04 +0800

    update app peosonal_homepage

--- move/js/presonal_homepage.js
+++ move/js/presonal_homepage.js
@@ -1,8 +1,10 @@
 $(document).ready(function() {
+    //查看更多
    $("#more").click(function () {
        $(this).toggleClass("active");
        $(".view-more").fadeToggle();
    });
+    //已关注or未关注
     $("#follow").click(function () {
         $(this).hide();
         $("#no-follow").fadeIn();
@@ -14,16 +16,19 @@ $(document).ready(function() {
         $("#more").addClass("active").removeClass("green");
         $(".view-more").fadeIn();
     });
+    //导航最右分享
     $("#share").click(function () {
 
         $(".share-bg").fadeIn();
         $(".share").slideDown();
     });
-    $(".share-bg").click(function () {
-        $(this).fadeOut();
-        $(".share").slideUp();
+    //分享灰色背景，取消按钮
+    $(".share-bg,.cancel").click(function () {
+        $(".share-bg").fadeOut();
+        $(".share,.share1,.share2").slideUp();
         $(".special_focus").fadeOut();
     });
+    //特别关注按钮（收藏）
     $(".glyphicon-star-empty").click(function () {
         $(this).toggle();
         $("#no-special").toggle();
@@ -34,8 +39,36 @@ $(document).ready(function() {
         $(this).toggle();
         $(".glyphicon-star-empty").toggle();
     });
+    //特别关注内容
     $(".special_focus").click(function () {
         $(this).fadeOut();
         $(".share-bg").fadeOut();
+    });
+    //爱心（热度）
+    $(".glyphicon-heart").click(function () {
+        $(this).toggleClass("red");
+        var num = $(this).parent().siblings("p.hot").find("span");
+        if ($(this).hasClass('red')){
+            num.text(parseInt(num.text())+1);
+        }
+        else {
+            num.text(parseInt(num.text())-1);
+        }
+    });
+    //赞
+    $(".glyphicon-thumbs-up").click(function () {
+        $(this).toggleClass("blue");
+    });
+    //图片分享
+    $(".glyphicon-option-horizontal").click(function () {
+
+        $(".share-bg").fadeIn();
+        $(".share1").slideDown();
+    });
+    //转载
+    $(".glyphicon-share").click(function () {
+
+        $(".share-bg").fadeIn();
+        $(".share2").slideDown();
     });
 });
